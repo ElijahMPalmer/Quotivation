1ea64cfac7f0b8b00268d644ded18e1038e57356
ElijahMPalmer <palmerelijah@rocketmail.com>
2022-04-09 22:04:59 -0700

    Implemented programming quotes Api and got the app to switch between the two databases of quotes correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import { Component } from 'react';
-import Fade from 'react-reveal/Fade';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { Component, useEffect, useState } from "react";
+import Fade from "react-reveal/Fade";
 
 export default class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      currentQuote: '',
-      currentAuthor: ''
+      currentCategory: "ourChoice",
+      currentQuotesArr: [],
+      currentQuote: "",
+      currentAuthor: "",
+      timerID: NaN,
     };
 
     this.slideShow = this.slideShow.bind(this);
-    
-  }
-  
-
-  componentDidMount(){
-    console.log('it ran')
-    const BASE_URL = window.location.origin;
-    fetch("http://localhost:3030/quotes", {mode:'cors'})
-    .then(res => res.json())
-    .then(
-      (result) => {
-        console.log("This is the result", result);
-        this.slideShow(result);
-      }
-    )
+    this.changeCategory = this.changeCategory.bind(this);
+    this.getQuotes = this.getQuotes.bind(this);
   }
 
-  slideShow(result){
+  componentDidMount() {
+    this.getQuotes();
+  }
+
+  getQuotes() {
+    switch (this.state.currentCategory) {
+      case "ourChoice":
+        console.log("made it to choice");
+        fetch("http://localhost:3030/quotes", { mode: "cors" })
+          .then((res) => res.json())
+          .then((result) => {
+            //console.log("This is the result", result);
+            this.setState(
+              {
+                currentQuotesArr: result,
+              },
+              () => {
+                //console.log("This is the Quotes ARR", this.state.currentQuotesArr)
+              }
+            );
+            this.slideShow();
+          });
+        break;
+      case "prog":
+        console.log("made it to prog");
+        fetch("https://programming-quotes-api.herokuapp.com/quotes", {
+          mode: "cors",
+        })
+          .then((res) => res.json())
+          .then((result) => {
+            this.setState({
+              currentQuotesArr: result,
+            });
+            this.slideShow();
+          });
+
+        break;
+    }
+  }
+
+  slideShow() {
+    if (this.state.timerID !== NaN) {
+      clearInterval(this.state.timerID);
+    }
+    let result = this.state.currentQuotesArr;
     let numQuotes = result.length;
     let index = 0;
-    this.setState({    
-      currentQuote: `${result[index].quote}`,
-      currentAuthor: `${result[index].author}`
-    })
+    let quote = "";
 
-    setInterval(() => {
-      this.setState({    
-        currentQuote: `${result[index].quote}`,
-        currentAuthor: `${result[index].author}`
-      })      
+    switch (this.state.currentCategory) {
+      case "ourChoice":
+        quote = result[index].quote;
+        break;
+      case "prog":
+        quote = result[index].en;
+        break;
+    }
+
+    this.setState({
+      currentQuote: quote,
+      currentAuthor: `${result[index].author}`,
+    });
+    index++;
+    let timer = setInterval(() => {
+      this.setState({
+        timerID: timer,
+      });
+      switch (this.state.currentCategory) {
+        case "ourChoice":
+          quote = result[index].quote;
+          break;
+        case "prog":
+          quote = result[index].en;
+          break;
+      }
+      this.setState(
+        {
+          currentQuote: quote,
+          currentAuthor: `${result[index].author}`,
+        },
+        () => {
+          console.log(
+            "Interval",
+            this.state.currentQuote,
+            this.state.currentAuthor
+          );
+        }
+      );
       index = (index + 1) % numQuotes;
-    }, 5000);
+    }, 15000);
+  }
+
+  changeCategory(category) {
+    console.log("Changing");
+    this.setState({ currentCategory: category }, () => {
+      console.log("Current Category", this.state.currentCategory);
+      this.getQuotes();
+    });
   }
 
   render() {
-    return(
-    <div className="App">
-      <Header/>
-      <Home
-        quote={this.state.currentQuote}
-        author={this.state.currentAuthor}
-      />
-    </div>
-    )};
+    return (
+      <div className="App">
+        <Header changeCategory={this.changeCategory} />
+        <Home
+          quote={this.state.currentQuote}
+          author={this.state.currentAuthor}
+        />
+      </div>
+    );
+  }
 }
-
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react'
 
-function Header() {
+function Header({changeCategory}) {
 
   return (
     <div className='container'>
@@ -8,13 +8,11 @@ function Header() {
           <img src="/images/Quotivation.svg" alt="" id='logo'/>
         </a>
         <div className='center-menu'>
-          <a href="#">Our Choice Quotes</a>
-          <a href="#">Stoicism Quotes</a>
+          <a href="#" onClick={() => changeCategory('ourChoice')}>Our Choice Quotes</a>
+          <a href="#" onClick={() => changeCategory('prog')} >Programming Quotes</a>
         </div>
 
-        <div className='right-menu'>
-            <a href="#">Widget Mode</a>
-        </div>
+       
         
     </div>
   )
