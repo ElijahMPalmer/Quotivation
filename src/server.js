2d16ce1e2529f1b738910515d9ba02eae25e80d1
ElijahMPalmer <palmerelijah@rocketmail.com>
2022-04-12 21:31:12 -0700

    Made changes to package.json

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 const { Pool } = require('pg');
 require("dotenv").config();
 const PORT = process.env.PORT;
