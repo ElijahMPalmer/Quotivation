e2351a795d3c1cd331c590ee3a858c2d3fd1e56c
ElijahMPalmer <palmerelijah@rocketmail.com>
2022-04-27 21:11:49 -0700

    Improved Mobile Responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ function Home({backgroundImg, quote, author}){
         <div className='wrap animate' id='wrap'>
                 <section id='quote-container'>
                     <h1>{ quote }</h1>
-                    <p id = 'author'>-{ author }</p>
+                    <p id='author'>-{ author }</p>
                 </section>
         </div>
     );
